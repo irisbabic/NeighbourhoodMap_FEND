7ce88a6659120a6591f3f05d601380e0ff85f88e
IrisBabic <iris.babic@gmail.com>
2018-09-01 10:56:09 +0200

    Adding locations

---
+++ src/locations.js
@@ -0,0 +1,10 @@
+let locations = [
+    {title: 'Bazeni Kantrida', location: {lat: 45.342433362542955, lng: 14.392433381103501},housenumber: '2' ,street:'Podkoludricu' ,city:'Rijeka' ,country: 'Croatia'},
+    {title: 'Konoba Tarsa', location: {lat: 45.331556, lng: 14.465086}, housenumber: '10' ,street:'J. Kulfaneka' ,city:'Rijeka' ,country: 'Croatia'},
+    {title: 'Odjel za informatiku', location: {lat: 45.328594, lng: 14.4645793}, housenumber: '2' ,street:'Ulica Radmile Matejčić' ,city:'Rijeka' ,country: 'Croatia'},
+    {title: 'Caffe Viva do Brasil', location: {lat: 45.340423, lng: 14.3814771}, housenumber: '1' ,street:'Pionirska ulica' ,city:'Rijeka' ,country: 'Croatia'},
+    {title: 'Caffe bar Morčić', location: {lat: 45.3374508, lng: 14.3055016}, housenumber: '12' ,street:'Radnička cesta' ,city:'Opatija' ,country: 'Croatia'},
+
+];
+
+module.exports = locations;
