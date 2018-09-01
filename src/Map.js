c52631b6b27fcc49788dafef92f3f373ecfaf9ff
IrisBabic <iris.babic@gmail.com>
2018-09-01 10:57:11 +0200

    Require location info from another file

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,8 @@
 import React, {Component} from 'react';
 import Search from './Search';
 import './App.css'
+
+let locations = require('./locations');
 /* eslint-disable no-undef */
 
 /**
@@ -14,15 +16,6 @@ state = {
     infoWindows: [],
     map,
 };
-
-    locations = [
-        {title: 'Bazeni Kantrida', location: {lat: 45.342433362542955, lng: 14.392433381103501},housenumber: '2' ,street:'Podkoludricu' ,city:'Rijeka' ,country: 'Croatia'},
-        {title: 'Konoba Tarsa', location: {lat: 45.331556, lng: 14.465086}, housenumber: '10' ,street:'J. Kulfaneka' ,city:'Rijeka' ,country: 'Croatia'},
-        {title: 'Odjel za informatiku', location: {lat: 45.328594, lng: 14.4645793}, housenumber: '2' ,street:'Ulica Radmile Matejčić' ,city:'Rijeka' ,country: 'Croatia'},
-        {title: 'Caffe Viva do Brasil', location: {lat: 45.340423, lng: 14.3814771}, housenumber: '1' ,street:'Pionirska ulica' ,city:'Rijeka' ,country: 'Croatia'},
-        {title: 'Caffe bar Morčić', location: {lat: 45.3374508, lng: 14.3055016}, housenumber: '12' ,street:'Radnička cesta' ,city:'Opatija' ,country: 'Croatia'},
-
-    ];
     errorArr = [];
     /**
      * @description Functions for filling infoWinfow with destination description
@@ -61,7 +54,7 @@ state = {
         let bounds;
         let markersObject = [];
         let infoWindows = [];
-        for (let location of this.locations) {
+        for (let location of locations) {
 
             fetch(`https://geocoder.api.here.com/6.2/geocode.json?app_id=32jh9NsjhShfRgz5bHu8&app_code=ZXLwdj55q46lMDUA-dru4g&searchtext=${location.housenumber}%20${location.street}%20${location.city}%20${location.country}`)
                 .then(response => response.json())
